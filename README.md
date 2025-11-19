9b224855fb2be01d1d1bc762aae1a070a4603954
arch jacob <jacobcwhubbard@gmail.com>
2025-11-19 21:38:07 +0000

    Created a simple project plan in main file

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # HTTPS [version?] Server
-I try to build a server using the socket API to teach me a bit about networking
+I try to build a server using the socket API to teach me a bit about networking.  
+I will first implement HTTP/0.9 and over time add more features.
---
+++ server.c
@@ -0,0 +1,12 @@
+/*
+Outline of the program:
+1. Create a Socket
+2. Bind the Socket to and address
+3. Listen on the Address
+4. Block on Accept until a connection is made
+5. Read on the connected Socket
+6. Respond
+7. Write back on the connected socket
+8. Close the connection
+9. Go back to blocking on accept
+*/
